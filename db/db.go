a40be8af291783ce4d290b982e8f5c6a26f97478
rizalta <rizalta00@gmail.com>
2025-09-11 23:53:44 +0530

    db package insert done
    will change storage to work with []byte keys instead of string

---
+++ db/db.go
@@ -0,0 +1,57 @@
+// Package db
+package db
+
+import (
+	"fmt"
+
+	"github.com/rizalta/toydb/catalog"
+	"github.com/rizalta/toydb/storage"
+	"github.com/rizalta/toydb/tuple"
+)
+
+type Database struct {
+	store   *storage.Store
+	catalog *catalog.Manager
+}
+
+func NewDatabase(dirPath string) (*Database, error) {
+	store, err := storage.NewStore(dirPath)
+	if err != nil {
+		return nil, err
+	}
+	catalog, err := catalog.NewManager(store)
+	if err != nil {
+		store.Close()
+		return nil, err
+	}
+
+	db := &Database{
+		store:   store,
+		catalog: catalog,
+	}
+
+	return db, nil
+}
+
+func createKey(tableID uint32, primaryKey tuple.Value) string {
+	return fmt.Sprintf("%d:%v", tableID, primaryKey)
+}
+
+func (db *Database) Insert(tableName string, row tuple.Tuple) error {
+	schema, err := db.catalog.GetTable(tableName)
+	if err != nil {
+		return err
+	}
+
+	data, err := tuple.Serialize(row, schema)
+	if err != nil {
+		return err
+	}
+
+	key := createKey(schema.ID, row[schema.PrimaryKeyIndex])
+	return db.store.Put(key, data)
+}
+
+func (db *Database) Close() error {
+	return db.store.Close()
+}
