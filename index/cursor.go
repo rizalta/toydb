0ffa5091c7ff0929c8504176be886bef2ce949cb
rizalta <rizalta00@gmail.com>
2025-10-02 00:21:08 +0530

    modified cursor to use ranged scans

--- index/cursor.go
+++ index/cursor.go
@@ -1,36 +1,67 @@
 package index
 
 import (
+	"bytes"
+	"sort"
+
 	"github.com/rizalta/toydb/pager"
 )
 
 type Cursor struct {
 	index  *Index
 	pageID pager.PageID
+	endKey []byte
 	keyNum int
 	isEnd  bool
 }
 
-func (idx *Index) NewCursor() (*Cursor, error) {
+func (idx *Index) NewCursor(startKey, endKey []byte) (*Cursor, error) {
+	if idx.root == 0 {
+		return &Cursor{isEnd: true}, nil
+	}
+
 	pageID := idx.root
 	n, _, err := idx.readNode(pageID)
 	if err != nil {
 		return nil, err
 	}
+	keyNum := 0
 
-	for n.nodeType == NodeTypeInternal {
-		pageID = n.children[0]
-		n, _, err = idx.readNode(pageID)
-		if err != nil {
-			return nil, err
+	if startKey == nil {
+		for n.nodeType == NodeTypeInternal {
+			pageID = n.children[0]
+			n, _, err = idx.readNode(pageID)
+			if err != nil {
+				return nil, err
+			}
+		}
+	} else {
+		for n.nodeType == NodeTypeInternal {
+			i := sort.Search(len(n.keys), func(j int) bool {
+				return bytes.Compare(n.keys[j], startKey) > 0
+			})
+			pageID = n.children[i]
+			n, _, err = idx.readNode(pageID)
+			if err != nil {
+				return nil, err
+			}
+		}
+
+		keyNum = sort.Search(len(n.keys), func(j int) bool {
+			return bytes.Compare(n.keys[j], startKey) >= 0
+		})
+		if keyNum >= len(n.keys) {
+			pageID = n.next
+			keyNum = 0
 		}
 	}
 
 	return &Cursor{
 		index:  idx,
 		pageID: pageID,
-		keyNum: 0,
-		isEnd:  len(n.keys) == 0,
+		keyNum: keyNum,
+		isEnd:  pageID == 0,
+		endKey: endKey,
 	}, nil
 }
 
@@ -44,7 +75,18 @@ func (c *Cursor) Next() ([]byte, uint64, error) {
 		return nil, 0, err
 	}
 
+	if len(n.keys) == 0 {
+		c.isEnd = true
+		return nil, 0, nil
+	}
+
 	key := n.keys[c.keyNum]
+
+	if c.endKey != nil && bytes.Compare(key, c.endKey) >= 0 {
+		c.isEnd = true
+		return nil, 0, nil
+	}
+
 	value := n.values[c.keyNum]
 
 	c.keyNum++
@@ -52,6 +94,7 @@ func (c *Cursor) Next() ([]byte, uint64, error) {
 		c.pageID = n.next
 		c.keyNum = 0
 	}
+
 	if c.pageID == 0 {
 		c.isEnd = true
 	}
